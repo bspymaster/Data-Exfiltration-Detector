9e0c0e4f8d832f0b4f44f7bb46ba18ff25cc2799
Ben Schwabe <bspymaster@gmail.com>
2016-01-12 15:47:11 -0600

    added ability to avoid files
    +avoids files that do not need to be processed (either already were
    processed or from today)
    today's file might not be complete so we cannot process that file yet.
    ~proper bro-cut command and arguments

--- NetworkHeuristics.py
+++ NetworkHeuristics.py
@@ -7,6 +7,7 @@
 import glob
 import os
 import subprocess as sp
+import datetime
 
 rootDirectory = "/full/path/to/folder/containing/folders/with/dated/logs/"
 
@@ -15,8 +16,16 @@ fileIterator = glob.iglob(rootDirectory+"*/dns.log") #rotates through all subdir
 
 #contains pre-processed copies of all the data the software will work with
 rawDataExists = os.path.isfile("raw_data.plog") #did the file exist
-runningDataFile = open("raw_data.plog","a+") #opens file for reading and appending
-#TODO: find a way to keep track of the last pieces of data logged and resume data calculations from that point
+runningDataFile = open("raw_data.plog","a") #opens file for appending
+
+#calculates the name of the folder that contains network data from the current day
+todayFolder = str(datetime.date.today()) #calculates today's date and saves as a string (yyyy-mm-dd)
+todayFolder = "".join(todayFolder.split("-")) #removes the hyphens from the name (yyyymmdd)
+
+TEMP = 0 #FIXME: change with last date processed into runningDataFile (set to 0 if reunningDataFile doesn't exist)
+
+#contains data on folders that contain data not needed (either already read or incomplete files)
+doNotReadDict = {"lastDateProcessed":TEMP,"today":todayFolder}
 
 #Return the sample arithmetic mean of data.
 def mean(data):
@@ -42,16 +51,20 @@ def stdev(data):
 
 while run: #runs while more files in the fileIterator queue
     try:
-        targetFile = fileIterator.next()
+        targetFile = fileIterator.next() #file with full path
+        targetFileList = targetFile.split("/")
+        targetFileDate = targetFileList[len(targetFileList-2)]
     except StopIteration:
         run = False #fileIterator threw an error for no more files to go through
     except:
         raise #just in case a different error is thrown, call it normally
     
-    if run:
-        runningDataFile.write(sp.check_output("cat {0} | bro-cut -d ts id.orig_h id.resp_h id.resp_p")) #cats the file and bro-cuts the appropriate data into the runningDataFile for use
-        #TODO: proper columns from file in bro-cuts
+    if run: #makes sure the loop is still active
+        if targetFileDate != doNotReadList["today"] and int(targetFileDate) > doNotReadDict["lastDateProcessed"]: #if folder of current file is not from today and is newer than the last date already processed
+            runningDataFile.write(sp.check_output("cat {0} | bro-cut -d ts id.orig_h id.resp_h id.resp_p orig_ip_bytes resp_ip_bytes".format(targetFile))) #cats the file and bro-cuts the appropriate data into the runningDataFile for use
+
+runningDataFile.close()
 
 #TODO: scan through the runningDataFile and calculate statistics
 
-runningDataFile.close()
+#TODO: write yesterday's date to a savefile as a "bookmark" for where the program left off
