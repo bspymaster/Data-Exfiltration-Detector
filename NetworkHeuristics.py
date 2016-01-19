94e6f218a7bd1407e7ed68d587575fb1beb480af
Ben Schwabe <bspymaster@gmail.com>
2016-01-19 15:36:01 -0600

    optimized data file crawling, prepared file for data processing
    -removed unnecessary methods (for now)
    ~changed file crawling to be sorted oldest to newest
    +added more comments for clarification

--- NetworkHeuristics.py
+++ NetworkHeuristics.py
@@ -4,67 +4,49 @@
 # 
 # Author:   Ben Schwabe
 
-import glob
+from glob import glob
 import os
 import subprocess as sp
 import datetime
+from calendar import monthrange
 
+#change this path to be the path to the server folder (one level out from the dated folders containing dns.log)
 rootDirectory = "/full/path/to/folder/containing/folders/with/dated/logs/"
 
-run = True
-fileIterator = glob.iglob(rootDirectory+"*/dns.log") #rotates through all subdirectories and grabs any file named "dns.log" in those subdirectories
+##############################
+#PARSE DATA INTO RUNNING FILE#
+##############################
+
+fileList = sorted(glob(rootDirectory+"*/dns.log")) #crawls through all subdirectories and grabs any file named "dns.log" in those subdirectories, puts it in a list, and sorts it smallest to largest (oldest to newest)
 
 #contains pre-processed copies of all the data the software will work with
-rawDataExists = os.path.isfile("raw_data.plog") #did the file exist
+#rawDataExists = os.path.isfile("raw_data.plog") #did the file exist
 runningDataFile = open("raw_data.plog","a") #opens file for appending
 
-#calculates the name of the folder that contains network data from the current day
+#calculates the name of the folder that contains network data from the current day (so it knows what folder to skip)
 todayFolder = str(datetime.date.today()) #calculates today's date and saves as a string (yyyy-mm-dd)
 todayFolder = "".join(todayFolder.split("-")) #removes the hyphens from the name (yyyymmdd)
 
-TEMP = 0 #FIXME: change with last date processed into runningDataFile (set to 0 if reunningDataFile doesn't exist)
+lastDate = -1 #FIXME: Change with calculations to pick up where it left off (keep as -1 if no file processed)
 
 #contains data on folders that contain data not needed (either already read or incomplete files)
-doNotReadDict = {"lastDateProcessed":TEMP,"today":todayFolder}
+doNotReadDict = {"lastDateProcessed":lastDate,"today":todayFolder}
 
-#Return the sample arithmetic mean of data.
-def mean(data):
-    n = len(data)
-    if n < 1:
-        raise ValueError('mean requires at least one data point')
-    return sum(data)/float(n)
-
-#Return sum of square deviations of sequence data.
-def sumSquare(data):
-    avg = mean(data)
-    sumData = sum((i-avg)**2 for i in data)
-    return sumData
-
-#Calculates the standard deviation.
-def stdev(data):
-    n = len(data)
-    if n < 2:
-        raise ValueError('variance requires at least two data points')
-    ss = sumSquare(data)
-    pvar = ss/n #the population variance
-    return pvar**0.5
-
-while run: #runs while more files in the fileIterator queue
-    try:
-        targetFile = fileIterator.next() #file with full path
-        targetFileList = targetFile.split("/")
-        targetFileDate = targetFileList[len(targetFileList-2)]
-    except StopIteration:
-        run = False #fileIterator threw an error for no more files to go through
-    except:
-        raise #just in case a different error is thrown, call it normally
-    
-    if run: #makes sure the loop is still active
-        if targetFileDate != doNotReadList["today"] and int(targetFileDate) > doNotReadDict["lastDateProcessed"]: #if folder of current file is not from today and is newer than the last date already processed
-            runningDataFile.write(sp.check_output("cat {0} | bro-cut -d ts id.orig_h id.resp_h id.resp_p orig_ip_bytes resp_ip_bytes".format(targetFile))) #cats the file and bro-cuts the appropriate data into the runningDataFile for use
+#crawls through sorted fileList
+for fileObj in fileList:
+    targetFileList = targetFile.replace("\\","/").split("/") #full path split into a list (handles Windows backslashes just in case)
+    targetFileDate = targetFileList[len(targetFileList-2)] #finds the date of the file using its parent folder
+    if targetFileDate != doNotReadList["today"] and int(targetFileDate) > doNotReadDict["lastDateProcessed"]: #if folder of current file is not from today and is newer than the last date already processed
+        runningDataFile.write(sp.check_output("cat {0} | bro-cut -d ts id.orig_h id.resp_h id.resp_p orig_ip_bytes resp_ip_bytes".format(targetFile))) #cats the file and bro-cuts the appropriate data into the runningDataFile for use
 
 runningDataFile.close()
 
+################################
+#CALCULATE AND ORGANIZE INTO DB#
+################################
+
 #TODO: scan through the runningDataFile and calculate statistics
+
+#TODO: remove everything except the current month's data from the runningDataFile (since we won't need it and it keeps storage costs down and processing speeds up)
 
 #TODO: write yesterday's date to a savefile as a "bookmark" for where the program left off
